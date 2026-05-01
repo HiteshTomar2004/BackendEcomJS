4e61b65d61f54bdf8e8aac3ef08c6b8c72e67b74
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-05-01 23:08:17 +0530

    updated products and delivery options apis to handle query parameter

--- src/controllers/deliveryOptions.js
+++ src/controllers/deliveryOptions.js
@@ -3,8 +3,21 @@ import { prisma } from "../config/db.js"
 export const getDeliveryOptions = async (req,res) =>{
     
     try{
+        const {expand} = req.query;
         const deliveryOptions = await prisma.deliveryOption.findMany();
-        res.status(200).json({success:true, data: deliveryOptions});
+
+        let responseData = deliveryOptions;
+
+        if(expand === 'estimatedDeliveryTime'){
+            responseData = deliveryOptions.map(option => {
+                return {
+                    ...option,
+                    estimatedDeliveryTimeMs: Date.now() + (option.deliveryDays * 24 * 60 * 60 * 1000)
+                }
+            })
+        }
+
+        res.status(200).json({success:true, data: responseData});
 
     }catch(error){
         res.status(500).json({success:false, error: error.message});
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -3,28 +3,43 @@ import { prisma } from '../config/db.js';
 //This is for get
 export const getAllProducts = async (req,res) =>{
     try{
-        const products = await prisma.product.findMany({
-            include: {
-                keywords: true
-            }
-        });
-
-        //implicit return => ({ }) same as => {return{ }}
-        const formatProducts = products.map((product) => {
-          return{  
-            id: product.id,
-            image: product.image,
-            name: product.name,
-            rating: {
-              stars: product.ratingStars,
-              count: product.ratingCount
-            },
-            priceCents: product.priceCents,
-            keywords: product.keywords.map((kw) => kw.word)
+      
+      const { search } = req.query;
+      let dbFilter={};
+      
+      //if there is a search param ?search= then check if there's a product with same name if not check if it has a same keyword
+      if(search){
+        dbFilter = {
+          OR:[
+            {name: {contains: search}},
+            { keywords: {some: { word: {contains: search}}}}
+          ]
+        };
+      }
+      //dbfilter is empty and returned with all the products if search is false otherwise it only has filtered products
+      const products = await prisma.product.findMany({
+          where: dbFilter,
+          include: {
+              keywords: true
           }
-        });
+      });
 
-        res.status(200).json({success:true, data:formatProducts});
+      //implicit return => ({ }) same as => {return{ }}
+      const formatProducts = products.map((product) => {
+        return{  
+          id: product.id,
+          image: product.image,
+          name: product.name,
+          rating: {
+            stars: product.ratingStars,
+            count: product.ratingCount
+          },
+          priceCents: product.priceCents,
+          keywords: product.keywords.map((kw) => kw.word)
+        }
+      });
+
+      res.status(200).json({success:true, data:formatProducts});
     }catch(error){
       res.status(500).json({success:false, error:error.message});
     }
